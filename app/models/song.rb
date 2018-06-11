a1e95a9b4a4db2070b6d30f3929cb78e67d3d58a
Chris Cardle <cdcardle@gmail.com>
2018-06-11 22:16:33 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -20,7 +20,7 @@ class Song < ActiveRecord::Base
   end
 
   def note_contents=(contents)
-    contents.each do |content|
+    contents.map do |content|
       note = Note.find_or_create_by(content: content)
       self.notes << note
     end
